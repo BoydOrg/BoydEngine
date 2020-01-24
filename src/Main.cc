0faf2299079fca78877db22fdaaafc91eeca896c
Paolo Jovon <paolo.jovon@gmail.com>
2020-01-24 23:40:04 +0000

    Made raylib log to the BoydEngine Log

--- src/Main.cc
+++ src/Main.cc
@@ -2,12 +2,32 @@
 
 #include "BoydEngine.hh"
 #include "Debug/Log.hh"
+using namespace boyd;
 
 #include "raylib.h"
+
+/// Raylib logging handler.
+static void raylibLog(int msgType, const char *fmt, va_list args)
+{
+    static const LogLevel RAYLIB_TO_BOYD_LOGLEVEL[] = {
+        LogLevel::Debug, // LOG_ALL
+        LogLevel::Debug, // LOG_TRACE
+        LogLevel::Debug, // LOG_DEBUG
+        LogLevel::Info,  // LOG_INFO
+        LogLevel::Warn,  // LOG_WARNING
+        LogLevel::Error, // LOG_ERROR
+        LogLevel::Crit,  // LOG_FATAL
+    };
+    static char buffer[256];
+    vsnprintf(buffer, sizeof(buffer), fmt, args);
+    buffer[255] = '\0';
+    Log::instance().log(RAYLIB_TO_BOYD_LOGLEVEL[msgType], "<raylib>", 0, buffer);
+}
 
 int main(void)
 {
     BOYD_LOG(Debug, "BoydEngine v{}.{}", BoydEngine_VERSION_MAJOR, BoydEngine_VERSION_MINOR);
+    SetTraceLogCallback(raylibLog);
 
     // Initialization
     //--------------------------------------------------------------------------------------
