7c5d19317ba3c75d29d4ad1cc9b29a22e33f9578
Enrico "erolm_a" Trombetta <trombetta.enricom@protonmail.com>
2020-01-24 17:17:37 +0000

    cmake stub

---
+++ src/BoydEngine.hh
@@ -0,0 +1,3 @@
+// link CMake's engine version to the source code
+#define BoydEngine_VERSION_MAJOR 0
+#define BoydEngine_VERSION_MINOR 1
---
+++ src/main.cxx
@@ -0,0 +1,7 @@
+#include <cstdio>
+#include "BoydEngine.hh"
+
+int main()
+{
+    printf("BoydEngine v%d.%d\n", BoydEngine_VERSION_MAJOR, BoydEngine_VERSION_MINOR);
+}
