72948a73c6fa20bdc1b7939208ad172bb47bef67
Enrico "erolm_a" Trombetta <trombetta.enricom@protonmail.com>
2020-01-26 19:49:15 +0000

    module hierarchy stub

---
+++ src/Modules/Gfx/Gfx.cpp
@@ -0,0 +1,32 @@
+#include "../../Core/Platform.hh"
+#include "../../Debug/Log.hh"
+
+/// TODO: add state transfer
+struct BoydGfxState {
+    /// TODO: add raylib core stuff...
+};
+
+inline BoydGfxState* GetState(void* state)
+{
+    return reinterpret_cast<BoydGfxState*>(state);
+}
+
+
+extern "C" {
+    BOYD_API void* BoydInit_Gfx()
+    {
+        BOYD_LOG(Info, "Started Gfx module");
+        return new BoydGfxState;
+    }
+
+    BOYD_API void BoydUpdate_Gfx(void* state)
+    {
+        BOYD_LOG(Info, "Update...");
+    }
+
+    BOYD_API void BoydHalt_Gfx(void* state)
+    {
+        BOYD_LOG(Info, "Halting Gfx module");
+        delete GetState(state);
+    }
+}
