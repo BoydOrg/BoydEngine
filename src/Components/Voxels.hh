854ff87c89d9f4054015b96e062a7f61238ae121
Paolo Jovon <paolo.jovon@gmail.com>
2020-01-31 18:54:26 +0000

    Add a voxel volume component

---
+++ src/Components/Voxels.hh
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "../Core/Platform.hh"
+#include <PolyVox/RawVolume.h>
+#include <cstdint>
+#include <memory>
+
+namespace boyd
+{
+namespace comp
+{
+
+/// The type of a single voxel.
+using Voxel = uint8_t;
+
+/// A voxel model.
+struct BOYD_API Voxels
+{
+    using Volume = PolyVox::RawVolume<Voxel>;
+
+    std::unique_ptr<Volume> volume;
+
+    Voxels(int32_t width, int32_t height, int32_t depth)
+    {
+        volume = std::make_unique<Volume>(PolyVox::Region({0, 0, 0}, {width, height, depth}));
+    }
+    ~Voxels() = default;
+
+    Voxels(const Voxels &toCopy) = delete;
+    Voxels &operator=(const Voxels &toCopy) = delete;
+    Voxels(Voxels &&toMove) = default;
+    Voxels &operator=(Voxels &&toMove) = default;
+};
+
+} // namespace comp
+} // namespace boyd
