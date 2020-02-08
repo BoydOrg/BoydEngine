3a222eec6ad33f4f29bcd6768048696c4bbf70a1
Paolo Jovon <paolo.jovon@gmail.com>
2020-02-08 01:13:34 +0000

    Remove Raylib out of Material.hh

--- src/Components/Material.hh
+++ src/Components/Material.hh
@@ -1,17 +1,16 @@
 #pragma once
 
 #include "../Core/Platform.hh"
-#include <raylib.h>
 
-using RaylibMaterial = Material;
 namespace boyd
 {
 namespace comp
 {
+
 struct BOYD_API Material
 {
-    RaylibMaterial material;
-    bool isDiffuseMap;
+    // TODO
 };
+
 } // namespace comp
 } // namespace boyd
