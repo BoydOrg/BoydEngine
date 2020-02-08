5a37ea921d7f10c87d283bc993eeb900ef2df194
Paolo Jovon <paolo.jovon@gmail.com>
2020-02-08 17:40:22 +0000

    Stubbed out a Material component suitable for graphics

--- src/Components/Material.hh
+++ src/Components/Material.hh
@@ -1,15 +1,45 @@
 #pragma once
 
 #include "../Core/Platform.hh"
+#include <glm/glm.hpp>
+#include <memory>
+#include <string>
+#include <unordered_map>
 
 namespace boyd
 {
 namespace comp
 {
 
+/// A texture.
+/// Not really a component, but used inside `comp::Material`s...
+struct BOYD_API Texture
+{
+    // FIXME IMPLEMENT
+};
+
+/// A shared pointer to texture data.
+using SharedTexture = std::shared_ptr<Texture>;
+
+/// A `comp::Material`'s parameter; corresponds more or less to a shader uniform.
+using MaterialParameter = std::variant<float, glm::vec2, glm::vec3, glm::vec4, glm::mat3, glm::mat4, SharedTexture>;
+
+/// A graphics material.
+/// Attach this to an entity with a Mesh and a Transform to make it renderable.
 struct BOYD_API Material
 {
-    // TODO
+    /// The parameters set in this material
+    std::unordered_map<std::string, MaterialParameter> parameters;
+
+    Material()
+        : parameters{}
+    {
+    }
+
+    Material(std::initializer_list<decltype(parameters)::value_type> parameters)
+        : parameters{parameters}
+    {
+    }
 };
 
 } // namespace comp
