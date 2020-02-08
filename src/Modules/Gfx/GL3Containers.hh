7a37585ef56265e44dae4aefd4bff9391497cd9e
Paolo Jovon <paolo.jovon@gmail.com>
2020-02-08 18:19:16 +0000

    Leaned out the definition of OpenGL RAII pointers (via inheritance)

--- src/Modules/Gfx/GL3Containers.hh
+++ src/Modules/Gfx/GL3Containers.hh
@@ -11,19 +11,42 @@ namespace boyd
 namespace gl3
 {
 
-/// A RAII shared pointer over an OpenGL buffer.
-struct SharedBuffer
+/// A RAII shared pointer over a OpenGL handle.
+struct SharedHandle
 {
+    /// Deletes the inner resource.
+    using Deleter = void (*)(GLuint *);
+
     std::shared_ptr<GLuint> handle;
 
-    /// Takes ownership of the given buffer.
-    explicit SharedBuffer(GLuint handle)
-        : handle{new GLuint(handle), &Deleter}
+protected:
+    /// Takes ownership of the given resource.
+    explicit SharedHandle(GLuint handle, Deleter deleter)
+        : handle{new GLuint(handle), deleter}
     {
     }
+
+public:
+    virtual ~SharedHandle() = default;
+
+    SharedHandle(const SharedHandle &toCopy) = default;
+    SharedHandle &operator=(const SharedHandle &toCopy) = default;
+    SharedHandle(SharedHandle &&toMove) = default;
+    SharedHandle &operator=(SharedHandle &&toMove) = default;
+
+    inline operator GLuint() const
+    {
+        return handle ? *handle : 0;
+    }
+};
+
+/// A RAII shared pointer over an OpenGL buffer.
+class SharedBuffer : public SharedHandle
+{
+public:
     /// Creates a new, empty buffer.
     explicit SharedBuffer()
-        : handle{new GLuint(0), &Deleter}
+        : SharedHandle(0, Deleter)
     {
         glGenBuffers(1, handle.get());
         if(!*handle)
@@ -32,6 +55,7 @@ struct SharedBuffer
             return;
         }
     }
+
     /// Creates a new buffer with the given data.
     SharedBuffer(GLenum target, GLsizeiptr size, const void *data, GLenum usage = GL_STATIC_DRAW)
         : SharedBuffer()
@@ -42,16 +66,6 @@ struct SharedBuffer
             glBufferData(target, size, data, usage);
             glBindBuffer(target, 0);
         }
-    }
-
-    SharedBuffer(const SharedBuffer &toCopy) = default;
-    SharedBuffer &operator=(const SharedBuffer &toCopy) = default;
-    SharedBuffer(SharedBuffer &&toMove) = default;
-    SharedBuffer &operator=(SharedBuffer &&toMove) = default;
-
-    inline operator GLuint() const
-    {
-        return handle ? *handle : 0;
     }
 
 private:
@@ -66,35 +80,20 @@ private:
 };
 
 /// A RAII shared pointer over an OpenGL VAO.
-struct SharedVertexArray
+class SharedVertexArray : public SharedHandle
 {
-    std::shared_ptr<GLuint> handle;
+public:
+    using SharedHandle::SharedHandle;
 
-    /// Takes ownership of the given vertex array.
-    explicit SharedVertexArray(GLuint handle)
-        : handle{new GLuint(handle), &Deleter}
-    {
-    }
     /// Creates a new vertex array.
     explicit SharedVertexArray()
-        : SharedVertexArray(0)
+        : SharedHandle(0, Deleter)
     {
         glGenVertexArrays(1, handle.get());
         if(!*handle)
         {
             BOYD_LOG(Warn, "Failed to create VAO");
-            return;
         }
-    }
-
-    SharedVertexArray(const SharedVertexArray &toCopy) = default;
-    SharedVertexArray &operator=(const SharedVertexArray &toCopy) = default;
-    SharedVertexArray(SharedVertexArray &&toMove) = default;
-    SharedVertexArray &operator=(SharedVertexArray &&toMove) = default;
-
-    inline operator GLuint() const
-    {
-        return handle ? *handle : 0;
     }
 
 private:
@@ -109,24 +108,13 @@ private:
 };
 
 /// A RAII shared pointer over an OpenGL texture.
-struct SharedTexture
+class SharedTexture : public SharedHandle
 {
-    std::shared_ptr<GLuint> handle;
-
+public:
     /// Takes ownership of the given texture.
     explicit SharedTexture(GLuint handle)
-        : handle{new GLuint(handle), &Deleter}
+        : SharedHandle(handle, Deleter)
     {
-    }
-
-    SharedTexture(const SharedTexture &toCopy) = default;
-    SharedTexture &operator=(const SharedTexture &toCopy) = default;
-    SharedTexture(SharedTexture &&toMove) = default;
-    SharedTexture &operator=(SharedTexture &&toMove) = default;
-
-    inline operator GLuint() const
-    {
-        return handle ? *handle : 0;
     }
 
 private:
@@ -140,7 +128,28 @@ private:
     }
 };
 
-/// A mesh that is loaded on the GPU.
+/// A RAII shared pointer over an OpenGL shader program.
+class SharedProgram : public SharedHandle
+{
+public:
+    /// Takes ownership of the given shader program.
+    explicit SharedProgram(GLuint handle)
+        : SharedHandle(handle, Deleter)
+    {
+    }
+
+private:
+    static void Deleter(GLuint *value)
+    {
+        if(value)
+        {
+            glDeleteProgram(*value);
+            delete value;
+        }
+    }
+};
+
+/// A RAII shared pointer over a mesh that is loaded on the GPU.
 struct Mesh
 {
     SharedVertexArray vao;
@@ -164,104 +173,6 @@ struct Mesh
     Mesh &operator=(Mesh &&tomove) = default;
 
     ~Mesh() = default;
-};
-
-/// A material that is loaded on the GPU.
-struct Material
-{
-    /// Name of the per-pass block of uniforms.
-    static constexpr const char *U_PERPASS_NAME = "Pass";
-    /// Name of the per-instance block of uniforms.
-    static constexpr const char *U_PERINSTANCE_NAME = "Instance";
-    /// Uniform block buffer bindpoints.
-    enum BlockBindings : GLuint
-    {
-        PERPASS = 1,
-        PERINSTANCE = 2,
-    };
-
-    /// Handle to the shader program.
-    GLuint program;
-    /// Contains <texture target, texture handle> pairs for the textures to bind, TEXTURE0..TEXTUREn.
-    std::vector<std::pair<GLenum, GLuint>> textures;
-
-    /// Creates a new, uninitialized material.
-    Material()
-        : program{0}, textures{}
-    {
-    }
-
-    /// Initializes a material given its already-loaded `program`, how many textures it will use,
-    /// and a (optional) handle to the per-pass uniform buffer.
-    Material(GLuint program, unsigned nTextures = 0)
-        : program{program}, textures{nTextures, {GL_TEXTURE_2D, 0}}
-    {
-        // Bind uniform blocks to their target locations
-        GLuint uPerPass = glGetUniformBlockIndex(program, U_PERPASS_NAME);
-        glUniformBlockBinding(program, uPerPass, PERPASS);
-        GLuint uPerInstance = glGetUniformBlockIndex(program, U_PERINSTANCE_NAME);
-        glUniformBlockBinding(program, uPerInstance, PERINSTANCE);
-    }
-
-    Material(const Material &toCopy) = delete;
-    Material &operator=(const Material &toCopy) = delete;
-
-    Material(Material &&toMove)
-        : program{toMove.program}, textures{std::move(toMove.textures)}
-    {
-        toMove.program = 0;
-    }
-    Material &operator=(Material &&toMove)
-    {
-        program = toMove.program;
-        textures = std::move(toMove.textures);
-
-        toMove.program = 0;
-
-        return *this;
-    }
-
-    /// Destroys the program (but NOT any uniform buffer or any of the textures!)
-    ~Material()
-    {
-        if(program)
-        {
-            glDeleteProgram(program);
-            program = 0;
-        }
-    }
-
-    /// Binds a pass of the shader program given the per-pass uniforms buffer.
-    void BeginPass(GLuint uPerPassBuffer)
-    {
-        glUseProgram(program);
-        glBindBufferBase(GL_UNIFORM_BUFFER, PERPASS, uPerPassBuffer);
-        for(GLuint i = 0; i < textures.size(); i++)
-        {
-            const auto &texPair = textures[i];
-            glActiveTexture(GL_TEXTURE0 + i);
-            glBindTexture(texPair.first, texPair.second);
-        }
-    }
-
-    /// Binds the per-instance uniform buffer into the shader program.
-    void Instance(GLuint uPerInstanceBuffer)
-    {
-        glBindBufferBase(GL_UNIFORM_BUFFER, PERINSTANCE, uPerInstanceBuffer);
-    }
-
-    /// Unbinds the program and all of the textures.
-    void EndPass()
-    {
-        glBindBufferBase(GL_UNIFORM_BUFFER, PERPASS, 0);
-        glBindBufferBase(GL_UNIFORM_BUFFER, PERINSTANCE, 0);
-        for(GLuint i = 0; i < textures.size(); i++)
-        {
-            glActiveTexture(GL_TEXTURE0 + i);
-            glBindTexture(textures[i].first, 0);
-        }
-        glUseProgram(0);
-    }
 };
 
 } // namespace gl3
