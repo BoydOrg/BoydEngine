72e207efe3528ab6d781538db69a0679f27ea021
Paolo Jovon <paolo.jovon@gmail.com>
2020-02-09 15:32:38 +0000

    Add a hasher to Versioned<T>, improve its Edit, add a ReferenceCount() method

--- src/Core/Versioned.hh
+++ src/Core/Versioned.hh
@@ -13,6 +13,8 @@ namespace boyd
 template <typename T>
 class Versioned
 {
+    friend struct std::hash<Versioned<T>>;
+
     struct Data
     {
         T value;
@@ -92,26 +94,24 @@ public:
     }
 
     /// Read-write access to the wrapped value: calls the given function, passing it a pointer to the wrapped value
-    /// (or null if no value is present), then increments the version number.
+    /// (or null if no value is present).
+    /// If the function returns true, considers the wrapped value modified and increments the version number; does nothing otherwise.
     /// Returns `this` for method chaining.
-    template <typename TFunction, typename = std::enable_if<std::is_invocable<TFunction, T *>::value>>
+    template <typename TFunction, typename = std::enable_if<std::is_invocable_r<bool, TFunction, T *>::value>>
     inline Versioned *Edit(TFunction &function)
     {
-        if(data)
+        T *valuePtr = data ? &data->value : nullptr;
+        bool modified = function(valuePtr);
+        if(modified)
         {
-            function(&data->value);
             data->version++;
-        }
-        else
-        {
-            function(nullptr);
         }
         return this;
     }
 
     /// Returns the current version number of the wrapped data.
     /// Returns 0 if the Versioned<T> is null.
-    inline unsigned version() const
+    inline unsigned Version() const
     {
         if(data)
         {
@@ -119,6 +119,32 @@ public:
         }
         return 0;
     }
+
+    /// Returns the current reference count of the wrapped data.
+    /// Returns 0 if the Versioned<T> is null.
+    inline long ReferenceCount() const
+    {
+        if(data)
+        {
+            return data.use_count();
+        }
+        return 0;
+    }
 };
 
 } // namespace boyd
+
+namespace std
+{
+
+template <typename T>
+struct hash<boyd::Versioned<T>>
+{
+    inline size_t operator()(const boyd::Versioned<T> &self) const
+    {
+        std::hash<decltype(self.data)> hasher;
+        return hasher(self.data);
+    }
+};
+
+} // namespace std
