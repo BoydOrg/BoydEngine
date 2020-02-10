87baeb34b780d093313896235d863d2f0fb536ca
Enrico "erolm_a" Trombetta <trombetta.enricom@protonmail.com>
2020-02-10 00:34:41 +0000

    Added rigid body support

--- src/Components/AllTypes.hh
+++ src/Components/AllTypes.hh
@@ -7,19 +7,21 @@
 #include "Camera.hh"
 #include "ComponentLoadRequest.hh"
 #include "Mesh.hh"
+#include "RigidBody.hh"
 #include "String.hh"
 #include "Transform.hh"
 
 /// Massive macro containing all types to register to the scripting system
 /// (as `BOYD_REGISTER_TYPE(<typename>)` entries)
-#define BOYD_REGISTER_ALLTYPES()                 \
-    BOYD_REGISTER_TYPE(boyd::comp::Transform)    \
-    BOYD_REGISTER_TYPE(boyd::comp::String)       \
-    BOYD_REGISTER_TYPE(boyd::comp::Camera)       \
-    BOYD_REGISTER_TYPE(boyd::comp::ActiveCamera) \
-    BOYD_REGISTER_TYPE(boyd::comp::Mesh)         \
-    BOYD_REGISTER_TYPE(boyd::comp::AudioSource)  \
-    BOYD_REGISTER_TYPE(boyd::comp::AudioClip)    \
-    BOYD_REGISTER_TYPE(boyd::comp::ComponentLoadRequest)
+#define BOYD_REGISTER_ALLTYPES()                         \
+    BOYD_REGISTER_TYPE(boyd::comp::Transform)            \
+    BOYD_REGISTER_TYPE(boyd::comp::String)               \
+    BOYD_REGISTER_TYPE(boyd::comp::Camera)               \
+    BOYD_REGISTER_TYPE(boyd::comp::ActiveCamera)         \
+    BOYD_REGISTER_TYPE(boyd::comp::Mesh)                 \
+    BOYD_REGISTER_TYPE(boyd::comp::AudioSource)          \
+    BOYD_REGISTER_TYPE(boyd::comp::AudioClip)            \
+    BOYD_REGISTER_TYPE(boyd::comp::ComponentLoadRequest) \
+    BOYD_REGISTER_TYPE(boyd::comp::RigidBody)
 
 // END
